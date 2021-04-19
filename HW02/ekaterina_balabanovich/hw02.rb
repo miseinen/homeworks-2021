83faef0e1192d8079432e32736dd626b0211d148
kot <miseinen22@gmail.com>
2021-04-19 22:16:22 +0300

    Add Attempt module. Add my_map, my_select, my_each methods.

---
+++ HW02/ekaterina_balabanovich/hw02.rb
@@ -0,0 +1,26 @@
+module Attempt
+  refine Array do
+    def my_map
+        mapped = []
+        for i in 0...self.length
+            mapped << yield(self[i]) if block_given?
+        end
+        mapped
+    end
+
+    def my_select
+        selected = []
+        for i in 0...self.length
+            selected << self[i] if block_given? && yield(self[i])
+        end
+        selected
+    end
+
+    def my_each
+        for i in 0...self.length
+            yield(self[i]) if block_given?
+        end
+        self
+    end
+  end
+end
