603a0af03020b4e05f7e668c0beaf6440eea17fc
kot <miseinen22@gmail.com>
2021-04-21 21:50:42 +0300

    Fix block checking in loop

--- HW02/ekaterina_balabanovich/array_methods.rb
+++ HW02/ekaterina_balabanovich/array_methods.rb
@@ -1,24 +1,30 @@
 module ArrayMethods
   refine Array do
     def my_map
+      return unless block_given?
+
       new_arr = []
       for i in 0...self.length
-        new_arr << yield(self[i]) if block_given?
+        new_arr << yield(self[i])
       end
       new_arr
     end
 
     def my_select
+      return unless block_given?
+
       new_arr = []
       for i in 0...self.length
-        new_arr << self[i] if block_given? && yield(self[i])
+        new_arr << self[i] if yield(self[i])
       end
       new_arr
     end
 
     def my_each
+      return unless block_given?
+
       for i in 0...self.length
-        yield(self[i]) if block_given?
+        yield(self[i])
       end
       self
     end
