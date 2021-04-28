fadb3355d53c54e5d31cf6a89010a85bb2979657
kot <miseinen22@gmail.com>
2021-04-28 18:01:02 +0300

    Add letter_digit_count implementation

--- HW03/ekaterina_balabanovich/balabanovich_hw03_task_4.rb
+++ HW03/ekaterina_balabanovich/balabanovich_hw03_task_4.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 def letter_digit_count(str)
-  str
+  letter = []
+  digit = []
+  str.chars.each do |a|
+    letter << a if %r{[[:alpha:]]}.match(a)
+    digit << a if %r{\d}.match(a)
+  end
+  puts "letters: #{letter.count}, digits: #{digit.count}"
 end
 
 letter_digit_count('hel2!lo')
