2a0bb9e93df0c947af51b9ec824946d13cfbc118
kot <miseinen22@gmail.com>
2021-04-28 16:40:38 +0300

    Add log_format implementation

--- HW03/ekaterina_balabanovich/balabanovich_hw03_task_2.rb
+++ HW03/ekaterina_balabanovich/balabanovich_hw03_task_2.rb
@@ -6,7 +6,14 @@ logs = '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTT
 10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290'
 
 def log_format(logs)
-  logs
+  logs.each_line do |line|
+    text = %r{/\w+/\d/\w+}.match(line).to_s.upcase
+    next if text.empty?
+
+    ip = %r{//(\d{1,3}\.){3}\d{1,3}//}.match(line).to_s
+    time = %r{\d{1,2}/\w+/\d{4}(:\d{2}){3}[[:space:]]\+\d{4}}.match(line).to_s
+    puts "#{time} FROM: #{ip} TO: #{text}"
+  end
 end
 
 log_format(logs)
