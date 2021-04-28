cd71d9e47f1f39ddaa64ca47648e7f5ff2864fce
kot <miseinen22@gmail.com>
2021-04-29 00:29:07 +0300

    Fix regex count in letter_digit_count

--- HW03/ekaterina_balabanovich/balabanovich_hw03_task_4.rb
+++ HW03/ekaterina_balabanovich/balabanovich_hw03_task_4.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 def letter_digit_count(str)
-  letter = []
-  digit = []
-  str.chars.each do |a|
-    letter << a if %r{[[:alpha:]]}.match(a)
-    digit << a if %r{\d}.match(a)
-  end
-  puts "letters: #{letter.count}, digits: #{digit.count}"
+  letter_regex = %r{[[:alpha:]]}
+  letter_count = str.scan(letter_regex).count
+  digit_regex = %r{\d}
+  digit_count = str.scan(digit_regex).count
+
+  puts "letters: #{letter_count}, digits: #{digit_count}"
 end
 
 letter_digit_count('hel2!lo')
