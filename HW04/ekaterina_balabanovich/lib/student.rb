749961162f544b16ae2a0c7a41db0baead561762
kot <miseinen22@gmail.com>
2021-04-29 17:07:20 +0300

    Add Student behaviour template

--- HW04/ekaterina_balabanovich/lib/student.rb
+++ HW04/ekaterina_balabanovich/lib/student.rb
@@ -7,4 +7,16 @@ class Student
     @name = name
     @surname = surname
   end
+
+  def notifications; end
+
+  def mark_as_read!; end
+
+  def homeworks; end
+
+  def to_work!; end
+
+  def add_answer!(homework, answer); end
+
+  def to_check!(homework); end
 end
