6e3d4cae4aff7c900007e88faf1bd70ac4dfe75c
kot <miseinen22@gmail.com>
2021-04-29 17:50:11 +0300

    Add Notification behaviour template

--- HW04/ekaterina_balabanovich/lib/notification.rb
+++ HW04/ekaterina_balabanovich/lib/notification.rb
@@ -1,3 +1,18 @@
+# frozen_string_literal: true
+
 class Notification
+  attr_reader :homework
+
+  def initialize(homework)
+    @homework = homework
+  end
+
+  def new; end
+
+  def done; end
+
+  def reject; end
+
+  def accept; end
 end
 
