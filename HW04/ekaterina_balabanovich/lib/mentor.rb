80ed0286cdbd8ff25bad063912462a99f03b8899
kot <miseinen22@gmail.com>
2021-04-29 17:11:26 +0300

    Add Mentor behaviour template

--- HW04/ekaterina_balabanovich/lib/mentor.rb
+++ HW04/ekaterina_balabanovich/lib/mentor.rb
@@ -7,4 +7,16 @@ class Mentor
     @name = name
     @surname = surname
   end
+
+  def add_homework(title:, description:, student:); end
+
+  def subscribe_to!(student); end
+
+  def notifications; end
+
+  def mark_as_read!; end
+
+  def reject_to_work!(homework); end
+
+  def accept!(homework); end
 end
