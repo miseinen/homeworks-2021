8bb04f0345ea1fdc0d4da18ab23c765d57a3c46f
kot <miseinen22@gmail.com>
2021-04-29 16:48:10 +0300

    Add constructor to Homework

--- HW04/ekaterina_balabanovich/lib/homework.rb
+++ HW04/ekaterina_balabanovich/lib/homework.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class Homework
+  attr_reader :title, :description
+
+  def initialize(title, description)
+    @title = title
+    @description = description
+  end
 end
