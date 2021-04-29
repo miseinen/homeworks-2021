e654862807e5b8d59e1f6bf3f8d561338c8291d7
kot <miseinen22@gmail.com>
2021-04-29 17:24:30 +0300

    Add Mentor subscribe_to! implementation

--- HW04/ekaterina_balabanovich/lib/mentor.rb
+++ HW04/ekaterina_balabanovich/lib/mentor.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 class Mentor
-  attr_reader :name, :surname
+  attr_reader :name, :surname, :students
 
   def initialize(name, surname)
     @name = name
     @surname = surname
+    @students = []
   end
 
   def add_homework(title:, description:, student:); end
 
-  def subscribe_to!(student); end
+  def subscribe_to!(student)
+    @students << student
+  end
 
   def notifications; end
 
