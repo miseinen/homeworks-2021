4eb4745ffff00c6205575a493fc55990a579ca9d
kot <miseinen22@gmail.com>
2021-05-04 02:09:53 +0300

    Add student's homework removing after accepting

--- HW04/ekaterina_balabanovich/lib/work_state/work_state_accepted.rb
+++ HW04/ekaterina_balabanovich/lib/work_state/work_state_accepted.rb
@@ -3,5 +3,10 @@
 class WorkStateAccepted < WorkState
   def process(members)
     homework.notify(members, :accept)
+    if members.is_a? Array
+      members.each { |a| a.delete_homework(homework) }
+    else
+      members.delete_homework(homework)
+    end
   end
 end
