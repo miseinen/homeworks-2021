0a5a1f8fcb108133214a0d2c72b11d6f98f7ddbe
kot <miseinen22@gmail.com>
2021-05-03 03:01:41 +0300

    Fix app process

--- HW04/ekaterina_balabanovich/app.rb
+++ HW04/ekaterina_balabanovich/app.rb
@@ -7,6 +7,7 @@ mentor = Mentor.new('Ivan', 'Stepanov')
 student = Student.new('Arina', 'Vesyolkova')
 
 homework = mentor.add_homework(title: 'HW01', description: 'Do something', student: student)
+puts 'Homework have been created'
 
 puts 'Student\'s notifications:'
 puts student.notifications
@@ -23,14 +24,17 @@ puts mentor.notifications
 mentor.mark_as_read!
 
 new_homework = mentor.add_homework(title: 'HW02', description: 'Do something else', student: student)
+puts 'Homework have been created'
 
 puts 'Student\'s notifications:'
 puts student.notifications
 student.mark_as_read!
-student.to_check!(homework)
 
-puts 'Homeworks before homework rejecting:'
-student.homeworks.each { |a| puts a.title }
+student.add_answer!(new_homework, 'another solving')
+student.to_check!(new_homework)
+
+puts 'Homeworks count before homework rejecting:'
+puts student.homeworks.count
 
 puts 'Mentor rejected homework'
 mentor.reject_to_work!(new_homework)
@@ -40,8 +44,9 @@ puts student.notifications
 student.mark_as_read!
 
 puts 'Homeworks after homework rejecting:'
-student.homeworks.count
-student.to_check!(homework)
+puts student.homeworks.count
+student.add_answer!(new_homework, 'fixed solving')
+student.to_check!(new_homework)
 
 puts 'Mentor accepted homework'
 mentor.accept!(new_homework)
@@ -49,5 +54,5 @@ mentor.accept!(new_homework)
 puts 'Student\'s notifications:'
 puts student.notifications
 
-puts 'Homeworks after homework accepting:'
-student.homeworks.count
+puts 'Homeworks count after homework accepting:'
+puts student.homeworks.count
