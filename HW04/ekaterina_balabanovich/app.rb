0b1eccb6e741adcc64eb46a02892e6fb97343608
kot <miseinen22@gmail.com>
2021-05-04 03:24:48 +0300

    Refactor app

--- HW04/ekaterina_balabanovich/app.rb
+++ HW04/ekaterina_balabanovich/app.rb
@@ -3,83 +3,87 @@ require_relative 'lib/mentor'
 require_relative 'lib/homework'
 require_relative 'lib/notification'
 
-mentor = Mentor.new('Ivan', 'Stepanov')
-mentor_new = Mentor.new('Trevor', 'Dutch')
-student = Student.new('Arina', 'Vesyolkova')
-student_new = Student.new('Stepan', 'Ivanov')
-student.subscribe_to(mentor)
+mentor_one = Mentor.new('Ivan', 'Stepanov')
+mentor_two = Mentor.new('Trevor', 'Dutch')
+student_one = Student.new('Arina', 'Vesyolkova')
+student_two = Student.new('Stepan', 'Ivanov')
+student_one.subscribe_to(mentor_one)
 
-homework = mentor.add_homework(title: 'HW01', description: 'Do something')
-puts 'Homework have been created.'
+def puts_with_delimiter(str)
+  puts "#{'-' * 30}\n#{str}"
+end
 
-puts 'Student\'s notifications:'
-puts student.notifications
-student.mark_as_read!
+homework_one = mentor_one.add_homework(title: 'HW01', description: 'Do something')
+puts_with_delimiter('HomeworkOne have been created.')
 
-puts 'Mentor assign homework to new student'
-mentor.assign(homework, student_new)
+puts_with_delimiter('StudentOne\'s notifications:')
+puts student_one.notifications
+student_one.mark_as_read!
 
-puts 'New student\'s notifications:'
-puts student_new.notifications
-student_new.mark_as_read!
+puts_with_delimiter('MentorOne assign HomeworkOne to new student.')
+mentor_one.assign(homework_one, student_two)
 
-puts 'Student\'s homeworks to work:'
-student.homeworks.each { |a| puts a.title }
+puts_with_delimiter('StudentTwo\'s notifications:')
+puts student_two.notifications
+student_two.mark_as_read!
 
-puts 'New student\'s homeworks to work:'
-student_new.homeworks.each { |a| puts a.title }
+puts_with_delimiter('StudentOne\'s homeworks to work:')
+student_one.homeworks.each { |a| puts a.title }
 
-mentor_new.join_to_check(homework)
-puts 'New mentor joined to review homework.'
+puts_with_delimiter('StudentTwo\'s homeworks to work:')
+student_two.homeworks.each { |a| puts a.title }
 
-student_new.add_answer!(homework, 'solving but another way')
-student_new.to_check!(homework)
-puts 'New student finished homework.'
+mentor_two.join_to_check(homework_one)
+puts_with_delimiter('New mentor joined to review homework.')
 
-student.add_answer!(homework, 'solving')
-student.to_check!(homework)
-puts 'Student finished homework.'
+student_two.add_answer!(homework_one, 'solving but another way')
+student_two.to_check!(homework_one)
+puts_with_delimiter('StudentTwo finished homework.')
 
-puts 'New mentor\s notifications'
-mentor_new.notifications.each { |a| puts a }
-mentor_new.mark_as_read!
+student_one.add_answer!(homework_one, 'solving')
+student_one.to_check!(homework_one)
+puts_with_delimiter('StudentOne finished homework.')
 
-puts 'Mentor\'s notifications:'
-mentor.notifications.each { |a| puts a }
-mentor.mark_as_read!
+puts_with_delimiter('MentorTwo\'s notifications')
+mentor_two.notifications.each { |a| puts a }
+mentor_two.mark_as_read!
 
-puts 'Mentor\'s homeworks to check:'
-mentor.homeworks.each { |a| puts a.title }
+puts_with_delimiter('MentorOne\'s notifications:')
+mentor_one.notifications.each { |a| puts a }
+mentor_one.mark_as_read!
 
-puts 'Mentor read answer:'
-mentor.homeworks.each do |a|
+puts_with_delimiter('MentorOne\'s homeworks to check:')
+mentor_one.homeworks.each { |a| puts a.title }
+
+puts_with_delimiter('MentorOne read answer:')
+mentor_one.homeworks.each do |a|
   puts a.title
   a.answers.each { |k, v| puts "#{k.surname}: #{v}" }
 end
 
-puts 'Mentor rejected  student\'s homework.'
-mentor.reject_to_work!(homework, student)
+puts_with_delimiter('MentorOne rejected  student\'s homework.')
+mentor_one.reject_to_work!(homework_one, student_one)
 
-puts 'Student\'s notifications:'
-puts student.notifications
-student.mark_as_read!
+puts_with_delimiter('StudentOne\'s notifications:')
+puts student_one.notifications
+student_one.mark_as_read!
 
-student.add_answer!(homework, 'fixed solving')
-student.to_check!(homework)
-puts 'Student fixed homework.'
+student_one.add_answer!(homework_one, 'fixed solving')
+student_one.to_check!(homework_one)
+puts_with_delimiter('StudentOne fixed homework.')
 
-puts 'Mentor\'s notifications:'
-puts mentor.notifications
-mentor.mark_as_read!
+puts_with_delimiter('MentorOne\'s notifications:')
+puts mentor_one.notifications
+mentor_one.mark_as_read!
 
-puts 'Mentor read answer:'
-mentor.homeworks.each do |a|
-  puts a.title
-  a.answers.each { |k, v| puts "#{k.surname}: #{v}" }
-end
+puts_with_delimiter('MentorOne read answer:')
+puts "#{student_one.surname}: #{homework_one.answers[student_one]}"
 
-puts 'Mentor accepted homework.'
-mentor.accept!(homework, student)
+puts_with_delimiter('MentorOne accepted homework.')
+mentor_one.accept!(homework_one, student_one)
 
-puts 'Student\'s notifications:'
-puts student.notifications
+puts_with_delimiter('StudentOne\'s notifications:')
+puts student_one.notifications
+
+puts_with_delimiter('StudentOne\'s homeworks amount to work:')
+puts student_one.homeworks.count
