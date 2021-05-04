3676a32e0712fe260a6f0305c9eb16269d38b74b
kot <miseinen22@gmail.com>
2021-05-04 07:41:32 +0300

    Remove redundant member subscribe method

--- HW04/ekaterina_balabanovich/lib/member.rb
+++ HW04/ekaterina_balabanovich/lib/member.rb
@@ -6,17 +6,12 @@ require_relative 'notification_receiver'
 class Member
   include TaskSolver
   include NotificationReceiver
-  attr_reader :name, :surname, :subscribers
+  attr_reader :name, :surname
 
   def initialize(name, surname)
     @name = name
     @surname = surname
     @homeworks = []
     @notifications = []
-    @subscribers = []
-  end
-
-  def subscribe_to(member)
-    member.subscribers << self
   end
 end
