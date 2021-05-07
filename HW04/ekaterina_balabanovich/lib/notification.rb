50549d206b4a046df5691e16d919c96e3c4b8af0
kot <miseinen22@gmail.com>
2021-05-08 00:54:11 +0300

    Fix Notification messages

--- HW04/ekaterina_balabanovich/lib/notification.rb
+++ HW04/ekaterina_balabanovich/lib/notification.rb
@@ -5,9 +5,25 @@ class Notification
 
   def initialize(homework)
     @homework = homework
-    @note = { new: "You got new homework: '#{homework.title}' to work",
-              done: "You got '#{homework.title} to check",
-              reject: "Your '#{homework.title}' have been rejected",
-              accept: "Your '#{homework.title} have been accepted" }
+    @note = { new: new_message,
+              done: done_message,
+              reject: reject_message,
+              accept: accept_message }
+  end
+
+  def new_message
+    "You got new homework: '#{homework.title}' to work"
+  end
+
+  def done_message
+    "You got '#{homework.title} to check"
+  end
+
+  def reject_message
+    "Your '#{homework.title}' have been rejected"
+  end
+
+  def accept_message
+    "Your '#{homework.title} have been accepted"
   end
 end
