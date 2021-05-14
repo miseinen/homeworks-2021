d69a9d5dbbd6159fb1435496bcc59f4dbb508801
kot <miseinen22@gmail.com>
2021-05-14 14:07:45 +0300

    Add let variables to spec

--- HW02/ekaterina_balabanovich/spec/array_methods_spec.rb
+++ HW02/ekaterina_balabanovich/spec/array_methods_spec.rb
@@ -5,40 +5,36 @@ require_relative '../array_methods'
 using ArrayMethods
 
 describe ArrayMethods do
+  let(:empty_array) { [] }
+  let(:array) { [1, 2, 3] }
+
   describe '#my_map' do
     context 'when array is empty' do
       it 'should be empty' do
-        array = []
-        expect(array.my_map(&:odd?)).to be_empty
+        expect(empty_array.my_map(&:odd?)).to be_empty
       end
     end
     context 'when array is not empty' do
       it 'should return array' do
-        array = [1, 2, 3]
         expect(array.my_map(&:odd?)).to eq([true, false, true])
       end
       it 'should return the same number of elements as the original array' do
-        array = [1, 2, 3, 5, 6]
         expect(array.my_map(&:odd?).count).to eq(array.count)
       end
     end
     context 'when block is not given' do
       it 'should return nil' do
-        array = [1, 2, 3]
         expect(array.my_map).to be nil
       end
     end
     context 'when block is given' do
       it 'should return sum of each elements if block uses sum' do
-        array = [1, 2, 3]
         expect(array.my_map { |a| a + 1 }).to eq([2, 3, 4])
       end
       it 'should return multiplication of elements if block uses multiplication' do
-        array = [1, 2, 3]
         expect(array.my_map { |a| a * 2 }).to eq([2, 4, 6])
       end
       it 'should not change the original array' do
-        array = [1, 2, 3]
         array.my_map { |a| a + 1 }
         expect(array).to eq([1, 2, 3])
       end
@@ -47,25 +43,21 @@ describe ArrayMethods do
   describe '#my_select' do
     context 'when array is empty' do
       it 'should be empty' do
-        array = []
-        expect(array.my_select(&:odd?)).to be_empty
+        expect(empty_array.my_select(&:odd?)).to be_empty
       end
     end
     context 'when array is not empty' do
       it 'should return array' do
-        array = [1, 2, 3]
         expect(array.my_select(&:odd?)).to eq([1, 3])
       end
     end
     context 'when block is not given' do
       it 'should return nil' do
-        array = [1, 2, 3]
         expect(array.my_select).to be nil
       end
     end
     context 'when block is given' do
       it 'should return only odd numbers if block checks odd numbers' do
-        array = [1, 2, 3, 4]
         expect(array.my_select(&:odd?)).to eq([1, 3])
       end
       it 'should return only zero numbers if block checks zero numbers' do
@@ -73,7 +65,6 @@ describe ArrayMethods do
         expect(array.my_select(&:zero?)).to eq([0, 0])
       end
       it 'should not change the original array' do
-        array = [1, 2, 3]
         array.my_select { |a| a + 1 }
         expect(array).to eq([1, 2, 3])
       end
@@ -82,37 +73,30 @@ describe ArrayMethods do
   describe '#my_each' do
     context 'when array is empty' do
       it 'should be empty' do
-        array = []
-        expect(array.my_each(&:odd?)).to be_empty
+        expect(empty_array.my_each(&:odd?)).to be_empty
       end
     end
     context 'when array is not empty' do
       it 'should return array' do
-        array = [1, 2, 3]
         expect(array.my_each(&:odd?)).to eq([true, false, true])
       end
       it 'should return the same number of elements as the original array' do
-        array = [1, 2, 3, 5, 6]
         expect(array.my_each(&:odd?).count).to eq(array.count)
       end
     end
     context 'when block is not given' do
       it 'should return nil' do
-        array = [1, 2, 3]
         expect(array.my_each).to be nil
       end
     end
     context 'when block is given' do
       it 'should return sum of each elements if block uses sum' do
-        array = [1, 2, 3]
         expect(array.my_each { |a| a + 1 }).to eq([2, 3, 4])
       end
       it 'should return multiplication of elements if block uses multiplication' do
-        array = [1, 2, 3]
         expect(array.my_each { |a| a * 2 }).to eq([2, 4, 6])
       end
       it 'should change the original array' do
-        array = [1, 2, 3]
         array.my_each { |a| a + 1 }
         expect(array).not_to eq([1, 2, 3])
       end
