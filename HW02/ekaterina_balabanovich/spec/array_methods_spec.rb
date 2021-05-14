de1aade71ae1718e79d68af425e6be7be24b1cfd
kot <miseinen22@gmail.com>
2021-05-14 13:44:44 +0300

    Add #my_select test

--- HW02/ekaterina_balabanovich/spec/array_methods_spec.rb
+++ HW02/ekaterina_balabanovich/spec/array_methods_spec.rb
@@ -44,4 +44,39 @@ describe ArrayMethods do
       end
     end
   end
+  describe '#my_select' do
+    context 'when array is empty' do
+      it 'should be empty' do
+        array = []
+        expect(array.my_select(&:odd?)).to be_empty
+      end
+    end
+    context 'when array is not empty' do
+      it 'should return array' do
+        array = [1, 2, 3]
+        expect(array.my_select(&:odd?)).to eq([1, 3])
+      end
+    end
+    context 'when block is not given' do
+      it 'should return nil' do
+        array = [1, 2, 3]
+        expect(array.my_select).to be nil
+      end
+    end
+    context 'when block is given' do
+      it 'should return only odd numbers if block checks odd numbers' do
+        array = [1, 2, 3, 4]
+        expect(array.my_select(&:odd?)).to eq([1, 3])
+      end
+      it 'should return only zero numbers if block checks zero numbers' do
+        array = [0, 2, 0, 4]
+        expect(array.my_select(&:zero?)).to eq([0, 0])
+      end
+      it 'should not change the original array' do
+        array = [1, 2, 3]
+        array.my_select { |a| a + 1 }
+        expect(array).to eq([1, 2, 3])
+      end
+    end
+  end
 end
