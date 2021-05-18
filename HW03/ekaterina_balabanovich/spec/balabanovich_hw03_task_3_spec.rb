f5855765514b04324369ceb2f2a41aae40c6cfdf
kot <miseinen22@gmail.com>
2021-05-18 16:02:28 +0300

    Fix context without variable in task3

--- HW03/ekaterina_balabanovich/spec/balabanovich_hw03_task_3_spec.rb
+++ HW03/ekaterina_balabanovich/spec/balabanovich_hw03_task_3_spec.rb
@@ -3,22 +3,22 @@
 require_relative '../balabanovich_hw03_task_3'
 
 RSpec.describe 'Task3' do
-  let(:logs) do
-    <<~LOGS
-      2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
-      2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
-      2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
-      2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
-      2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
-      2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
-      2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
-    LOGS
-  end
-
   describe '#extract_core_log' do
     subject { extract_core_log(logs) }
 
     context 'when logs are not empty' do
+      let(:logs) do
+        <<~LOGS
+          2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+          2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+          2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
+          2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+          2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+          2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+          2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
+        LOGS
+      end
+
       let(:expected_output) do
         <<~LOGS
           2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
@@ -52,6 +52,18 @@ RSpec.describe 'Task3' do
     end
 
     context 'when logs contain more than one lines' do
+      let(:logs) do
+        <<~LOGS
+          2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+          2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+          2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
+          2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+          2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+          2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+          2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
+        LOGS
+      end
+
       it { is_expected.to eq([49.1]) }
     end
 
