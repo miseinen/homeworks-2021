19d60a7fdb67d3050577690f24d69ee8a38923ca
miseinen <miseinen22@gmail.com>
2021-05-31 14:33:53 +0300

    add Answer spec

---
+++ HW04/ekaterina_balabanovich/spec/lib/answer_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/answer'
+require_relative '../../lib/student'
+
+RSpec.describe Answer do
+  describe 'attributes' do
+    context 'when :student is "John" and :answer is "answer"' do
+      let(:student) { Student.new('John', 'Doe') }
+      subject { described_class.new(student, 'answer') }
+      let(:expected_attributes) { { student: student, answer: 'answer' } }
+
+      it { is_expected.to have_attributes(expected_attributes) }
+    end
+
+    context 'when :student is empty and :answer is empty' do
+      subject { described_class.new('', '') }
+      let(:expected_attributes) { { student: '', answer: '' } }
+
+      it { is_expected.to have_attributes(expected_attributes) }
+    end
+  end
+
+  describe '#show' do
+    context 'when :student is "John Doe" and :answer is "my answer"' do
+      let(:student) { Student.new('John', 'Doe') }
+      subject { -> {described_class.new(student, 'answer').show } }
+      let(:expected_output) { "Doe: answer\n" }
+
+      it { is_expected.to output(expected_output).to_stdout }
+    end
+  end
+end
